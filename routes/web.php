8f7afd5d9e8134a9877e33bfb5df1831c81adf6e
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-28 18:15:49 -0600

    routes and css

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\StaffController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('home');
-});
+Route::get('/', [HomeController::class,'index'])->name('home.index');
+
+Route::resource('clients', ClientController::class)->name('index', 'clients.index');
+Route::resource('orders', OrderController::class)->name('index', 'orders.index');
+Route::resource('staff', StaffController::class)->name('index', 'staff.index');
