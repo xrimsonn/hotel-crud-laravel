92238e3251b7ceaa28849b9de700516f4e52f3cf
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-28 18:14:20 -0600

    CRUD controllers

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClientFormRequest;
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return view(
+      'clients.index',
+      [
+        'clients' => Client::all()
+      ]
+    );
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    return view('clients.create');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(ClientFormRequest $request)
+  {
+    $data = $request->validated();
+    Client::create($data);
+    return redirect()->route('clients.index');
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Client $client)
+  {
+    return view('clients.show', [
+      'client' => $client
+    ]);
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Client $client)
+  {
+    return view('clients.edit', [
+      'client' => $client
+    ]);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(ClientFormRequest $request, Client $client)
+  {
+    $data = $request->validated();
+    $client->update($data);
+
+    return redirect()->route('clients.show', $client->id);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Client $client)
+  {
+    $client->delete();
+    return redirect()->route('clients.index');
+  }
+}
