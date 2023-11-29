2344d9146e5f86cbd0cf670f5ac9595e7862ca54
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-28 21:12:31 -0600

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
-@section('title', 'Inicio')
+@section('title', 'OneTree Hotel')
 
 @section('content')
-  <em>Placeholder</em>
-@endsection
+<hgroup>
+
+  <h3>Descubre Onetree by Halton</h3>
+  <p>Ubicado en el corazón de la ciudad, Onetree by Halton combina la elegancia contemporánea con la calidez de la hospitalidad. Nuestro hotel ofrece una escapada única para aquellos que buscan una experiencia inolvidable.</p>
+</hgroup>
+<img src="https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/1b/3f/f0/thelocal-hotels-mazatlan.jpg" alt="Vista al mar desde la piscina del hotel.">
+  @endsection
